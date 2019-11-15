6f3e569a3d7ca443950a46ef326abf53bd1a5c4d
ToniNichev <toni.nichev@gmail.com>
2019-11-15 13:15:26 -0500

    Formatting

--- text-box-with-dropdown.js
+++ text-box-with-dropdown.js
@@ -76,87 +76,87 @@ class TextboxWithDropdown extends HTMLElement {
   }
 
   keyUp(e) {
-  if(e.keyCode == 13) {
-    this.rowSelected(e);
-    return;
-  }
-  if(e.keyCode == '38' || e.keyCode == '40') {
-    this.arrowUpDown(e);
-    return;
-  }
-  var prefix = this._shadowRoot.getElementById('textfield').value;
-  if(prefix == '') {
-    this._shadowRoot.getElementById('drawer').innerHTML = '';
-    this.selectedIndex = 0;
-    this.filteredWordsCount = 0;
-    return;
-  }
-  if(this.isDrawerOpen == true)
-    return;
-  var words = this.filterWords(prefix);
-  this._shadowRoot.getElementById('drawer').innerHTML = words.join('');
-  // attach the events
-  var c = 0;
-  words.map(function(row, i) {
-    var row = this._shadowRoot.getElementById('row-' + i);
-    row.addEventListener("mousemove", function() {
-      this.selectHighlight(i);
-    }.bind(this));
-    row.addEventListener("click", function(e) {
+    if(e.keyCode == 13) {
       this.rowSelected(e);
+      return;
+    }
+    if(e.keyCode == '38' || e.keyCode == '40') {
+      this.arrowUpDown(e);
+      return;
+    }
+    var prefix = this._shadowRoot.getElementById('textfield').value;
+    if(prefix == '') {
+      this._shadowRoot.getElementById('drawer').innerHTML = '';
+      this.selectedIndex = 0;
+      this.filteredWordsCount = 0;
+      return;
+    }
+    if(this.isDrawerOpen == true)
+      return;
+    var words = this.filterWords(prefix);
+    this._shadowRoot.getElementById('drawer').innerHTML = words.join('');
+    // attach the events
+    var c = 0;
+    words.map(function(row, i) {
+      var row = this._shadowRoot.getElementById('row-' + i);
+      row.addEventListener("mousemove", function() {
+        this.selectHighlight(i);
+      }.bind(this));
+      row.addEventListener("click", function(e) {
+        this.rowSelected(e);
+      }.bind(this));
     }.bind(this));
-  }.bind(this));
-  // select first row if any
-  if(words.length > 0)
-    this._shadowRoot.getElementById('row-0').classList.add("selectedRow");
-}  
+    // select first row if any
+    if(words.length > 0)
+      this._shadowRoot.getElementById('row-0').classList.add("selectedRow");
+  }  
 
-arrowUpDown(e) {
-  if(this.selectedIndex > -1)
-    this._shadowRoot.getElementById('row-' + this.selectedIndex).classList.remove("selectedRow");    
-  if(e.keyCode == '38' && this.selectedIndex > 0) {
-    // arrow up
-    this.selectedIndex --;
+  arrowUpDown(e) {
+    if(this.selectedIndex > -1)
+      this._shadowRoot.getElementById('row-' + this.selectedIndex).classList.remove("selectedRow");    
+    if(e.keyCode == '38' && this.selectedIndex > 0) {
+      // arrow up
+      this.selectedIndex --;
+    }
+    else if(e.keyCode == '40' && this.selectedIndex < this.filteredWordsCount - 1) {
+      // arrow down
+      this.selectedIndex ++;
+    }
+    this._shadowRoot.getElementById('row-' + this.selectedIndex).classList.add("selectedRow");
+    e.preventDefault();
+    return false;
   }
-  else if(e.keyCode == '40' && this.selectedIndex < this.filteredWordsCount - 1) {
-    // arrow down
-    this.selectedIndex ++;
+
+  rowSelected(e) {
+    if(this.filteredWordsCount == 0)
+      return;
+    this._shadowRoot.getElementById('textfield').value = this._shadowRoot.getElementById('row-' + this.selectedIndex).innerText;
+    this._shadowRoot.getElementById('drawer').innerHTML = '';
+    this.filteredWordsCount = 0;
+    this.selectedIndex = 0;
+    e.preventDefault();
+    return false;    
   }
-  this._shadowRoot.getElementById('row-' + this.selectedIndex).classList.add("selectedRow");
-  e.preventDefault();
-  return false;
-}
 
-rowSelected(e) {
-  if(this.filteredWordsCount == 0)
-    return;
-  this._shadowRoot.getElementById('textfield').value = this._shadowRoot.getElementById('row-' + this.selectedIndex).innerText;
-  this._shadowRoot.getElementById('drawer').innerHTML = '';
-  this.filteredWordsCount = 0;
-  this.selectedIndex = 0;
-  e.preventDefault();
-  return false;    
-}
-
-//business logic comes here. Bad idea!. Separate this in a diffrent class. But for the simplicity of the example we will keep it here.
-filterWords(prefix) {
-  prefix = prefix.toLowerCase();
-  var result = [];
-  for(var i=0; i < this.dictionary.length;i ++) {
-    var wordArray = this.dictionary[i].toLowerCase();
-    for(var j=0; j < prefix.length && j < wordArray.length; j ++) {
-      if(prefix[j] != wordArray[j]) {
-        break;
+  //business logic comes here. Bad idea!. Separate this in a diffrent class. But for the simplicity of the example we will keep it here.
+  filterWords(prefix) {
+    prefix = prefix.toLowerCase();
+    var result = [];
+    for(var i=0; i < this.dictionary.length;i ++) {
+      var wordArray = this.dictionary[i].toLowerCase();
+      for(var j=0; j < prefix.length && j < wordArray.length; j ++) {
+        if(prefix[j] != wordArray[j]) {
+          break;
+        }
+      }
+      if(prefix.length == j) {
+        var wordRow = '<p id="row-' + result.length + '">' + this.dictionary[i] + '</p>';
+        result.push(wordRow);
       }
     }
-    if(prefix.length == j) {
-      var wordRow = '<p id="row-' + result.length + '">' + this.dictionary[i] + '</p>';
-      result.push(wordRow);
-    }
-  }
-  this.filteredWordsCount = result.length;
-  return result;
-}  
+    this.filteredWordsCount = result.length;
+    return result;
+  }  
 
 }
 
